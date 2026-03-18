6b57ae0c567da67e580b982206b433c599376e00
longlexiang-rgb <longlexiang@gmail.com>
2026-03-18 19:18:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // 配置文件
-export const API_BASE_URL = 'http://localhost:3000'; // 后端API地址
-export const USE_MOCK = true; // 是否使用模拟数据（当后端不可用时）
+export const API_BASE_URL = 'https://ecg-show.vercel.app'; // 后端API地址
+export const USE_MOCK = false; // 是否使用模拟数据（当后端不可用时）
