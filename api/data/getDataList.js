40cf440401bb919c004229af2114c3fa898a8198
longlexiang-rgb <longlexiang@gmail.com>
2026-03-20 13:49:03 +0800

    Implement CORS support in getDataList.js
    Added CORS headers and OPTIONS request handling.

--- api/data/getDataList.js
+++ api/data/getDataList.js
@@ -2,6 +2,16 @@ const { connectDB, WaveData } = require('../db');
 const { verifyToken, getTokenFromHeader } = require('../auth/verifyToken');
 
 module.exports = async (req, res) => {
+   // 添加 CORS 头部
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // 处理 OPTIONS 请求
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     try {
         // 验证token
         const token = getTokenFromHeader(req);
